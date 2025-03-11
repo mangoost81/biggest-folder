f31daeae2a9826b261342c77892b195f0935c7a0
Mangoost <mangoost81@gamil.com>
2025-03-12 00:40:54 +0400

    add method getfoldersize

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String folderpath = "/home/user/GeekBox/Homeworks";
+        File file = new File(folderpath);
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
+            return folder.length();
+        }
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
